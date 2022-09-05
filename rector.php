4c9f66b2caf635b1f3ab4f15e6c8401401fd546d
Bob van de Vijver <bobvandevijver@hotmail.com>
2022-09-05 09:51:58 +0200

    Fix rector config

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([__DIR__ . '/src']);
     $rectorConfig->importNames();
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
